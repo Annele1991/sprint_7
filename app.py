d814f8a792bb9ea079a25868aa4cf740c7b70296
Annele1991 <annele.aceves@gmail.com>
2025-06-18 11:19:46 -0400

    update

--- app.py
+++ app.py
@@ -16,3 +16,17 @@ if hist_button:  # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+hist_button = st.button('Construir gráfico de dispersión')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.header('Precio vs odometro')
+    st.write(
+        'Creación de un gráfico de dispersión')
+
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
